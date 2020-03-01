a730db024561a3118fe292fc03c5639370289bba
notaimless <61641096+notaimless@users.noreply.github.com>
2020-02-29 22:16:36 -0500

    Update CameraShake.cs

--- source/scripting_v3/RDR2/CameraShake.cs
+++ source/scripting_v3/RDR2/CameraShake.cs
@@ -8,5 +8,13 @@ namespace RDR2
 	public enum CameraShake
 	{
 		Hand,
+		NONE,
+		SMALL_EXPLOSION_SHAKE,
+		MEDIUM_EXPLOSION_SHAKE,
+		LARGE_EXPLOSION_SHAKE,
+		JOLT_SHAKE,
+		VIBRATE_SHAKE,
+		WOBBLY_SHAKE,
+		DRUNK_SHAKE,
 	}
 }
